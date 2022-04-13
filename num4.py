7217b41a0414bd648bbd6a330a706a0bfac33bb2
Денис С <96002480+Ycon2@users.noreply.github.com>
2022-04-13 15:37:37 +0300

    Update num4.py

--- num4.py
+++ num4.py
@@ -1,44 +1,60 @@
-#Введение пользователем размеров матрицы
+# Преобразование вводимых значений из строковых в числовые с помощью метода integerate
+def integerate(matrix):
+    for i in range(len(matrix)):
+        matrix[i] = int(matrix[i])
+    returnmatrix
+
+
+# Введение пользователем размеров матрицы
 size1 = input('Введите размер матрицы №1 через пробел - ').split()
 size2 = input('Введите размер матрицы №2 через пробел - ').split()
+# Задание размера матриц и проверка возможности умножения
+size1 = integerate(size1)
+size2 = integerate(size2)
 
-#Преобразование вводимых значений из строковых в числовые с помощью метода integerate
-class matrix():
-    def integerate(self, matrix):
-        for i in range(len(matrix)):
-            matrix[i] = int(matrix[i])
-        return matrix
 
-#Задание размера матриц и проверка возможности умножения
-size1 = matrix().integerate(size1)
-size2 = matrix().integerate(size2)
-if size1[0] == size2[1]:
-    print('Матрицы возможно умножить, процесс...')
-    print(f'Размер матрицы №1 - {size1[0]}x{size1[1]}')
-    print(f'Размер матрицы №2 - {size2[0]}x{size2[1]}')
-    #a и b - наше дано, c - наше искомое. Объявление их пустыми для последующей работы
-    a = []
-    b = []
-    c = []
-    #Заполнение матрицы a
-    for i in range(size1[0]):
-        if size1[1] == 1:
-            a_el = int(input(f'Введите элемент строки №{i + 1} матрицы a '))
-        else:
-            a_el = input(f'Введите строку №{i + 1} матрицы a через пробел, количество элементов - {size1[1]} ').split()
-            a_el = matrix().integerate(a_el)
-        a.append(a_el)
-    #Заполнение матрицы b
-    for i in range(size2[0]):
-        if size2[1] == 1:
-            b_el = int(input(f'Введите элемент строки №{i + 1} матрицы b '))
-        else:
-            b_el = input(f'Введите строку №{i + 1} матрицы b через пробел, количество элементов - {size2[1]} ').split()
-            b_el = matrix().integerate(b_el)
-        b.append(b_el)
-    #Начало вычислений
-    #Впиздошу сюда умножение матрицы когда приеду, ориентировочно это понедельник (может быть вечер воскресения)
-    #будьте вольны пробовать решать сами, меня не ждите, думайте тоже
-    #(Решите пятое задание методом Гаусса, 3 дня уже жду -^-)
+def calculate(matrix1, matrix2):
+    if matrix1[1] == matrix2[0]:
+        print('Матрицы возможно умножить, процесс...')
+print(f'Размер матрицы №1 - {matrix1[0]}x{matrix2[1]}')
+print(f'Размер матрицы №2 - {matrix1[0]}x{matrix2[1]}')
+        # a и b - наше дано, c - наше искомое. Объявление их пустыми для последую-щей работы
+a = []
+b = []
+c = []
+        # Заполнение матрицы a
+for i in range(matrix1[0]):
+            if matrix1[1] == 1:
+                a_el = int(input(f'Введите элемент строки №{i + 1} матрицы a '))
+                a_el = list(a_el)
+            else:
+                a_el = input(
+                    f'Введите строку №{i + 1} матрицы a через пробел, количество элементов - {size1[1]} ').split()
+                a_el = integerate(a_el)
+            a.append(a_el)
+        # Заполнение матрицы b
+for i in range(matrix2[0]):
+            if matrix2[1] == 1:
+                b_el_num = int(input(f'Введите элемент строки №{i + 1} матрицы b '))
+                b_el = [b_el_num]
+            else:
+                b_el = input(
+                    f'Введите строку №{i + 1} матрицы b через пробел, количество элементов - {matrix2[1]} ').split()
+                b_el = integerate(b_el)
+            b.append(b_el)
+        # Начало вычислений
+m = len(a)
+        n = len(b)
+        k = len(b[0])
+
+        c = [[None for __ in range(k)] for __ in range(m)]  # Пустой структурированный список
+        for i in range(m):  # Замена None на настоящие значения матрицы
+for j in range(k):
+                c[i][j] = sum(a[i][k] * b[k][j] for k in range(n))
+        returnc
 else:
-    print('Невозможно умножить матрицы, неподходящие размеры')
+print('Невозможно умножить матрицы, неподходящие размеры')
+
+
+result = calculate(size1, size2)
+print('Полученная матрица:\n', result)
